b53a1bc971980c976c4a5d1c6efbc6992c16b93b
Himanshu Verma <kumarsurajverma6001@gmail.com>
2025-02-23 02:46:18 +0530

    backend fix 4

--- frontend/src/hooks/GetAllJobs.jsx
+++ frontend/src/hooks/GetAllJobs.jsx
@@ -5,22 +5,16 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const GetAllJobs = () => {
   const dispatch = useDispatch();
-  const { searchedQuery } = useSelector((state) => state.job);
-
+  const { searchedQuery } = useSelector((store) => store.job);
   useEffect(() => {
-    const getAllJobs = async () => {
+    const fetchAllJobs = async () => {
       try {
         const res = await axios.get(
-          `https://job-portal-kpvv.onrender.com/job/alljobs?keyword=${searchedQuery}`,
-          {
-            withCredentials: true,
-            headers: {
-              Authorization: `Bearer ${token}`, // ✅ Token header add kiya
-              'Content-Type': 'application/json',
-            },
-          }
+          `https://job-portal-kpvv.onrender.com/job/alljobs?keyword=${
+            searchedQuery || ''
+          }`,
+          { withCredentials: true }
         );
-
         if (res.data.success) {
           dispatch(setAllJobs(res.data.jobs));
         }
@@ -28,7 +22,7 @@ const GetAllJobs = () => {
         console.log(error);
       }
     };
-    getAllJobs();
+    fetchAllJobs();
   }, []);
 };
 
